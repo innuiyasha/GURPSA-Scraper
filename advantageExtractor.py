5a81b52418edb11c763cf118b90866b386fedca5
Innuiyasha <spencerbriere@gmail.com>
2016-12-16 14:10:08 -0700

    advantage names parsed correctly

--- advantageExtractor.py
+++ advantageExtractor.py
@@ -24,16 +24,41 @@ with open(sys.argv[1]) as f:
 	print("Parsing for advantages")
 	
 	for linenum in range(len(book)):
-		if(re.match("[0-9]+\s(point)", book[linenum]) and (not re.search("[;,]",book[linenum]))):
+		if(re.match("[0-9]+\s(point)", book[linenum]) and (not re.search("[;\.,]", book[linenum]))):
 			target.write("<advantage>\n")
 			
-			if(re.match("[a-zA-Z]", book[linenum - 1])):
-				target.write("\t<name>\n\t\t" + book[linenum - 1] + "\n\t</name>\n\t\t")
+			if(re.match("[a-zA-Z0-9°/]", book[linenum - 1]) and re.search("[a-zA-Z]", book[linenum - 1])):
+				target.write("\t<name>\n\t\t" + book[linenum - 1] + "\t</name>\n")
 			else:
-				target.write("\t<name>\n\t\t" + book[linenum - 2] + "\n\t</name>\n\t\t")
+				target.write("\t<name>\n\t\t" + re.sub("[\n\r]", "", book[linenum - 2]) + " " + re.sub("[\n\r\s]+", "", book[linenum - 1]) + "\n\t</name>\n")
 			
 			target.write("</advantage>\n")
-
+		elif(re.match("^[0-9]+(,\s+[0-9]+|\sto\s[0-9]+|\sor\s[0-9]+)+", book[linenum])):
+			target.write("<advantage>\n")
+			
+			if(not re.match("[0-9]", book[linenum - 1]) and re.search("[a-zA-Z]", book[linenum - 1])):
+				target.write("\t<name>\n\t\t" + book[linenum - 1] + "\t</name>\n")
+			else:
+				target.write("\t<name>\n\t\t" + re.sub("[\n\r]", "", book[linenum - 2]) + " " + re.sub("[\n\r\s]+", "", book[linenum - 1]) + "\n\t</name>\n")
+			
+			target.write("</advantage>\n")
+		elif(re.match("Variable", book[linenum])):
+			target.write("<advantage>\n")
+			
+			if(re.match("[a-zA-Z0-9°]", book[linenum - 1]) and re.search("[a-zA-Z]", book[linenum - 1])):
+				target.write("\t<name>\n\t\t" + book[linenum - 1] + "\t</name>\n")
+			else:
+				target.write("\t<name>\n\t\t" + re.sub("[\n\r]", "", book[linenum - 2]) + " " + re.sub("[\n\r\s]+", "", book[linenum - 1]) + "\n\t</name>\n")
+			
+			target.write("</advantage>\n")
+		elif(re.match("see.*p.\s+[0-9]+[\s\n\r]+", book[linenum])):
+			if(re.match("[a-zA-Z0-9°]", book[linenum - 1])):
+				target.write("\t<name>\n\t\t" + book[linenum - 1] + "\t</name>\n")
+			else:
+				target.write("\t<name>\n\t\t" + re.sub("[\n\r]", "", book[linenum - 2]) + " " + re.sub("[\n\r\s]+", "", book[linenum - 1]) + "\n\t</name>\n")
+			
+			target.write("</advantage>\n")
+			
 
 target.close()
 print("-----Done-----")
